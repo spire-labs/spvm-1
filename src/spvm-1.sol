88c0fbce08113c96bbe9c3c287daf42ad35032d9
Matthew <matthew8edelen@gmail.com>
2024-03-30 08:58:12 -0400

    add blocks

--- src/spvm-1.sol
+++ src/spvm-1.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.0;
 
+import "openzeppelin-contracts/utils/cryptography/SignatureChecker.sol";
+
 /// @title Spire PoC Virtual Machine - version 1 - interpreter
 /// @author mteam
 contract SPVM {
@@ -44,8 +46,7 @@ contract SPVM {
         uint16 amount;
     }
 
-    // TODO: remove external permissions, should only be called by block executor and after signer check
-    function executeRawTransaction(bytes memory rawTx) external {
+    function executeRawTransaction(bytes memory rawTx) internal {
         TransactionContent memory txContent = abi.decode(
             rawTx,
             (TransactionContent)
@@ -115,5 +116,82 @@ contract SPVM {
             revert("Invalid transaction type");
         }
         return true;
+    }
+
+    // recover signer of transaction from signature
+    function validateSignature(
+        bytes32 transaction_hash,
+        bytes memory signature,
+        address expected_signer
+    ) internal pure returns (bool) {
+        return SignatureChecker.isValidSignatureNow(
+            expected_signer,
+            transaction_hash,
+            signature
+        );
+    }
+
+    function executeTx(
+        Transaction tx
+    ) internal {
+        bytes32 txHash = keccak256(abi.encode(tx.txContent));
+        require(
+            txHash == tx.transactionHash,
+            "Invalid transaction hash"
+        );
+        require(
+            validateSignature(tx.transactionHash, tx.signature, txContent.from),
+            "Invalid signature"
+        );
+        executeRawTransaction(abi.encode(txContent));
+    }
+
+    struct Transaction {
+        TransactionContent txContent;
+        bytes32 transactionHash;
+        bytes signature;
+    }
+
+    function executeBlockTransactions(
+        Transaction[] memory txs
+    ) internal {
+        for (uint i = 0; i < txs.length; i++) {
+            executeTx(txs[i]);
+        }
+    }
+
+    struct Block {
+        Transaction[] transactions;
+        bytes32 blockHash;
+        bytes32 parentHash;
+        uint32 blockNumber;
+    }
+
+    // all historical blocks
+    Block[] public blocks; 
+
+    // TODO: add permissions
+    function proposeBlock (
+        Block memory block
+    ) external {
+        // get most recent block
+        Block memory lastBlock = blocks[blocks.length - 1];
+
+        require(
+            block.blockHash == keccak256(block.parentHash + abi.encode(block.transactions)),
+            "Invalid block hash"
+        );
+        require(
+            block.blockNumber == lastBlock.blockNumber + 1,
+            "Invalid block number"
+        );
+        require(
+            block.parentHash == lastBlock.blockHash,
+            "Invalid parent hash"
+        );
+
+        blocks.push(block);
+
+        executeBlockTransactions(block.transactions);
     }
 }
